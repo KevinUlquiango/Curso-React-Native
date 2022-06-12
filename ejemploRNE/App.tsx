f1f85bbb02d529e11e59eaf6a692709adbe28724
Kevin Ulquiango <78171886+KevinUlquiango@users.noreply.github.com>
2022-06-12 18:27:11 -0500

    KUG-Uso del input de elements

--- ejemploRNE/App.tsx
+++ ejemploRNE/App.tsx
@@ -1,15 +1,22 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { Button, Icon } from "@rneui/base";
-import React from "react";
+import { Alert, StyleSheet, Text, View } from "react-native";
+import { Button, Icon, Input } from "@rneui/base";
+import { Icon as CIcon } from "@rneui/themed";
+import React, { useState } from "react";
 
 export default function App() {
+  const [name, setName] = useState<any>();
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <Button
         icon={{
           name: "apple",
+          /*
+          Familia a la que pertenece
+          consultar mas iconos en https://oblador.github.io/react-native-vector-icons/
+          */
+
           type: "font-awesome",
           size: 15,
           color: "white"
@@ -20,7 +27,23 @@ export default function App() {
           marginHorizontal: 50,
           marginVertical: 10
         }}
-      ></Button>
+      />
+
+      <Button
+        onPress={() => {
+          Alert.alert("Hola", "w");
+        }}
+      >
+        <Icon name='home' color='white' />
+      </Button>
+      <CIcon name='g-translate' color='#00aced' />
+      <Input
+        value={name}
+        onChangeText={setName}
+        label={name}
+        placeholder='Ingrese el nombre'
+      />
+      <Text>{name}</Text>
       <StatusBar style='auto' />
     </View>
   );
