32ca0b7791d179112896c4e28e358900ea4f2547
Kevin Ulquiango <78171886+KevinUlquiango@users.noreply.github.com>
2022-06-12 18:26:36 -0500

    KUG-Forma de implementar icon

--- ejemploRNE/App.tsx
+++ ejemploRNE/App.tsx
@@ -1,19 +1,26 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { Button } from "@rneui/base";
+import { Button, Icon } from "@rneui/base";
+import React from "react";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <Button
+        icon={{
+          name: "apple",
+          type: "font-awesome",
+          size: 15,
+          color: "white"
+        }}
         title={"React Native Elements"}
         containerStyle={{
           width: 200,
           marginHorizontal: 50,
           marginVertical: 10
         }}
-      />
+      ></Button>
       <StatusBar style='auto' />
     </View>
   );
--- ejemploRNE/README.md
+++ ejemploRNE/README.md
@@ -11,3 +11,20 @@
 `npm install react-native-vector-icons`
 
 `npx react-native link react-native-vector-icons`
+
+**Página para por ver los iconos disponibles a usar**
+
+`https://oblador.github.io/react-native-vector-icons/`
+
+```
+import { Button, Icon } from "@rneui/base";
+<Button
+icon={{
+          name: "apple",
+          type: "font-awesome",
+          size: 15,
+          color: "white"
+        }}
+title={"React Native Elements"}
+/>
+```
