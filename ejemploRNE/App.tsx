509591ce1df5d91081b54fb5106b431bba68ea6f
Kevin Ulquiango <78171886+KevinUlquiango@users.noreply.github.com>
2022-06-11 21:36:37 -0500

    KUG-Ejercicio React Elements

---
+++ ejemploRNE/App.tsx
@@ -0,0 +1,29 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Button } from "@rneui/base";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button
+        title={"React Native Elements"}
+        containerStyle={{
+          width: 200,
+          marginHorizontal: 50,
+          marginVertical: 10
+        }}
+      />
+      <StatusBar style='auto' />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
